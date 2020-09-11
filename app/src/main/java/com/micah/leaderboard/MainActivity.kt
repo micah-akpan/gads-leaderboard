266aba457b67717d01fbe8ecfc0a88279ccaf5cb
Micah Akpan <mickey4sure2k7@gmail.com>
2020-09-11 22:29:41 +0100

    Add submission activity

--- app/src/main/java/com/micah/leaderboard/MainActivity.kt
+++ app/src/main/java/com/micah/leaderboard/MainActivity.kt
@@ -2,12 +2,20 @@ package com.micah.leaderboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.micah.leaderboard.R
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.lb_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true;
     }
 }
